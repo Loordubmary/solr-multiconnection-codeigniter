f828d40365759e5cd06ab17ee61dd3c77742b097
loordumary@usistech.com <loordumary@usistech.com>
2019-05-09 14:51:21 +0530

    Project folder - commit

---
+++ application/config/solarium.php
@@ -0,0 +1,22 @@
+<?php
+	$config['endpoint1'] = array(
+		'endpoint' => array(
+			'localhost' => array(
+				'host' => 'localhost',
+				'port' => 8983,
+				'path' => '/solr/',
+				'core' => 'core1'
+			)
+		)
+	);
+	$config['endpoint2'] = array(
+		'endpoint' => array(
+			'localhost' => array(
+				'host' => 'localhost',
+				'port' => 8983,
+				'path' => '/solr/',
+				'core' => 'core2'
+			)
+		)
+	);
+?>
---
+++ application/config/twig.php
@@ -0,0 +1,7 @@
+<?php
+	$config['twig']['template_dir'] = VIEWPATH;
+	$config['twig']['template_ext'] = 'php';
+	$config['twig']['environment']['autoescape'] = TRUE;
+	$config['twig']['environment']['cache'] = FALSE;
+	$config['twig']['environment']['debug'] = FALSE;
+?>
